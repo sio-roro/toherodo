69702339b12bb48ea8b5bf635200e858c05dae2f
sio-maker <sio0040maker@gmai.com>
2020-10-23 15:22:18 +0900

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+// expressの呼び込み
+const express = require('express');
+// PORTの設定
+// もしenvにportが確立したらそのポートを、していなかったら8080を使う
+const port = process.env.PORT || 5000;
+// express()のインスタンス
+const app = express();
+
+// どこにアクセスさせるかの設定
+// distフォルダ：コンパイルされたファイルが入る場所
+app.use(express.static(__dirname + "/dist/"));
+
+// SPAの時、ルーティングがうまくいかない時があるので以下の設定
+// これをしていないと、https://~~~/aboutとかに行った時にリロードするとエラーがでる
+// SPAだとaboutファイルを直で読み込んでいないから
+// これでルート以外でリロードしてもindex.htmlを読み込んでちゃんとルーティングをしてくれる
+// /.*/で全てのルートを指定。req(request), res(response)
+app.get(/.*/, function(req, res) {
+    res.sendfile(__dirname + "/dist/index.html");
+});
+
+// 一番上で指定したportをlisten
 app.listen(port);
-console.log('server started '+ port);
+
+// 動いてるかどうかconsoleで見る
+console.log("Server is up!!");
