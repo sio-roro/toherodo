8d4414896d1641e447cdf1289e06f27da2ff3e88
sio-maker <sio0040maker@gmai.com>
2020-10-23 14:05:33 +0900

    Herokuセットアップ

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,15 @@
 
 import Dnd1 from './components/dnd1.vue'
 
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
+
 
 export default {
   name: 'App',
